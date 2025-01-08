f77d4d5838207d6a4f44bab05502848c7ce98c18
Alexey Leshchenko <leshchenko@gmail.com>
2025-01-08 22:48:01 +0300

    Enhance Mixpanel tracking with error handling
    - Introduced a new decorator `track_with_error_handling` to wrap the Mixpanel track method, providing robust error logging.
    - Integrated Yandex logging for improved error reporting.
    - Updated the `mp.track` method to utilize the new error handling functionality, ensuring better resilience during tracking operations.

--- src/app/common/mp.py
+++ src/app/common/mp.py
@@ -1,6 +1,11 @@
+import functools
 import os
 
 from mixpanel import Mixpanel
+
+from .yandex_logging import get_yandex_logger
+
+logger = get_yandex_logger(__name__)
 
 
 class SilentMixpanel:
@@ -12,6 +17,21 @@ class SilentMixpanel:
 
 
 mp = Mixpanel(os.getenv("MIXPANEL_PROJECT_TOKEN"))
+
+
+def track_with_error_handling(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            logger.error(f"Error in mp.track: {e}", exc_info=True)
+            # Optionally, you can add more error handling logic here
+
+    return wrapper
+
+
+mp.track = track_with_error_handling(mp.track)
 
 
 def mute_mp_for_tests():
