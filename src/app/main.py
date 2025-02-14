92ba4ee4295f3b909b376b8bb2725e86bf0c9406
Alexey Leshchenko <leshchenko@gmail.com>
2025-02-15 01:55:04 +0400

    Simplify

--- src/app/main.py
+++ src/app/main.py
@@ -13,17 +13,12 @@ from .logging_setup import setup_logging
 setup_logging()
 logger = logging.getLogger(__name__)
 
-# Import all handlers to register them with the dispatcher
-from .handlers import *
-
-logger.debug("Handlers imported")
-
 # Start the server
 from aiohttp import web
 
+# Import all handlers to register them with the dispatcher
+from .handlers import *
 from .server import app  # noqa
-
-logger.debug("Server imported")
 
 if __name__ == "__main__":
     web.run_app(app, host="0.0.0.0", port=8080)
