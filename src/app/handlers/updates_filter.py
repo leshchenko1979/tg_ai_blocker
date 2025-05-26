206fa4f9bbad5ed9fe0c44ea5870432057e1b9ee
Alexey Leshchenko <leshchenko@gmail.com>
2025-05-26 16:18:04 +0300

    Enhance message filtering logic in updates_filter.py for group chats
    - Update filter_handle_message to include detailed conditions for processing messages in group and supergroup chats.
    - Ensure messages are only processed if they are not from admins, not edited, and not service messages.
    - Allow messages containing text or media, and refine logic for handling replies based on sender type.
    - Improve code readability with comprehensive comments outlining the filtering criteria.

--- src/app/handlers/updates_filter.py
+++ src/app/handlers/updates_filter.py
@@ -1,16 +1,25 @@
 from aiogram import F
 from aiogram.filters import and_f, or_f
 
-# Сообщение должно уходить в обработку handle_message, если:
+# Фильтр для передачи сообщения в handle_message:
+# 1. Только для групп и супергрупп
+# 2. Только если отправитель не админ
+# 3. Сообщение не отредактировано
+# 4. Не сервисное сообщение (новые участники, смена фото и т.д.)
+# 5. Сообщение содержит текст или медиа
+# 6. Разрешаем:
+#    - Сообщения не-ответы
+#    - Ответы вне треда
+#    - Ответы в треде, если отправитель — бот
+
 filter_handle_message = and_f(
+    # 1. Только групповые чаты
     F.chat.type.in_(["group", "supergroup"]),
+    # 2. Не админ
     ~F.from_user.is_admin,
-    or_f(
-        ~F.reply_to_message,
-        and_f(F.reply_to_message, ~F.reply_to_message.message_thread_id),
-    ),
+    # 3. Не отредактированное сообщение
     ~F.edited_message,
-    # Исключаем все типы сервисных сообщений
+    # 4. Не сервисные сообщения
     ~F.new_chat_member,
     ~F.new_chat_members,
     ~F.left_chat_member,
@@ -22,7 +31,7 @@ filter_handle_message = and_f(
     ~F.channel_chat_created,
     ~F.message_auto_delete_timer_changed,
     ~F.pinned_message,
-    # Проверяем наличие текста или медиа-контента
+    # 5. Содержит текст или медиа
     or_f(
         F.text,
         F.photo,
@@ -34,5 +43,16 @@ filter_handle_message = and_f(
         F.animation,
         F.audio,
         F.story,
+    ),
+    # 6. Логика для ответов:
+    #    - Не ответ
+    #    - Ответ вне треда
+    #    - Ответ в треде, если отправитель — бот
+    or_f(
+        ~F.reply_to_message,
+        and_f(F.reply_to_message, ~F.reply_to_message.message_thread_id),
+        and_f(
+            F.reply_to_message, F.reply_to_message.message_thread_id, F.from_user.is_bot
+        ),
     ),
 )
